29230d8c7314b016139c529cc855baf42edfb4eb
devXodia <youstylez@gmail.com>
2024-04-01 19:26:16 +0200

    added forget password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { PasswordResetComponent } from './components/auth/password-reset/password-reset.component';
 
-export const routes: Routes = [{ path: '', component: LoginComponent }];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'auth/password-reset', component: PasswordResetComponent },
+];
---
+++ src/app/components/auth/password-reset/password-reset.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-password-reset',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './password-reset.component.html',
+  styleUrl: './password-reset.component.scss',
+})
+export class PasswordResetComponent {}
