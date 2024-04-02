6776c974cf797cd80b90a0002ee2eb9253b85208
devXodia <youstylez@gmail.com>
2024-04-02 14:02:42 +0200

    added register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { PasswordResetComponent } from './components/auth/password-reset/password-reset.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'auth/password-reset', component: PasswordResetComponent },
+  { path: 'password-reset', component: PasswordResetComponent },
+  { path: 'register', component: RegisterComponent },
 ];
