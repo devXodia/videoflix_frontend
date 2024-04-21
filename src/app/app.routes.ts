d1c463c24cbd1d314bfbcb9043f57928ffa92238
devXodia <youstylez@gmail.com>
2024-04-21 11:47:38 +0200

    feat: adding verify component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { PasswordResetComponent } from './components/auth/password-reset/password-reset.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { VerifyEmailComponent } from './components/auth/verify-email/verify-email.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'password-reset', component: PasswordResetComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'verify/:token', component: VerifyEmailComponent },
 ];
---
+++ src/app/components/auth/verify-email/verify-email.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-email.component.html',
+  styleUrl: './verify-email.component.scss',
+})
+export class VerifyEmailComponent {
+  verifySuccess: boolean = false;
+  verifyError: boolean = false;
+}
