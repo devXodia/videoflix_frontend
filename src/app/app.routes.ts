61d2a85112bd8bf9e29f016b706eee718b0c90fc
devXodia <youstylez@gmail.com>
2024-04-27 11:50:42 +0200

    feat: working on home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PasswordResetComponent } from './components/auth/password-reset/passwor
 import { RegisterComponent } from './components/auth/register/register.component';
 import { VerifyEmailComponent } from './components/auth/verify-email/verify-email.component';
 import { SetNewPasswordComponent } from './components/auth/password-reset/set-new-password/set-new-password.component';
+import { HomeComponent } from './components/main/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'password-reset', component: PasswordResetComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'verify/:token', component: VerifyEmailComponent },
-  {path: 'set-password/:token', component: SetNewPasswordComponent}
+  {path: 'set-password/:token', component: SetNewPasswordComponent},
+  {path: 'home', component: HomeComponent}
 ];
