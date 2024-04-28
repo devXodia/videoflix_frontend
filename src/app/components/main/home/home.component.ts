5fbb1e724559885c75e5ba8d0e48d930341a5e60
devXodia <youstylez@gmail.com>
2024-04-28 16:49:28 +0200

    feat: working on home layout and adding scrolling to movie list

--- src/app/components/main/home/home.component.ts
+++ src/app/components/main/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  @ViewChild('movieListContainer') movieListContainer!: ElementRef<HTMLDivElement>;
+
+  scrollAmount: number = 800;
+
+  scrollLeft(){
+    if (this.movieListContainer) {
+      this.movieListContainer.nativeElement.scrollTo({
+        left: this.movieListContainer.nativeElement.scrollLeft - this.scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  scrollRight(){
+    if (this.movieListContainer) {
+      this.movieListContainer.nativeElement.scrollTo({
+        left: this.movieListContainer.nativeElement.scrollLeft + this.scrollAmount,
+        behavior: 'smooth'
+      });
+    }
+  }
+  
 
 }
